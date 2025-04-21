29a7e38e7042a4d05fb6d1aed6d034c5e13f4849
Samuel Mathes <samuel.mathes@gmx.de>
2025-04-21 15:53:51 +0200

    Fixed Gradle config

--- backend/src/test/kotlin/ApiResourceTest.kt
+++ backend/src/test/kotlin/ApiResourceTest.kt
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.MediaType
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
